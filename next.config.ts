1c34555ea10a840f125c04acc1bf8b5033aa5521
EfraelTino <efrael2001@gmail.com>
2025-01-03 00:17:50 -0500

    puppeter config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['livdata.online'],
-    
   },
-  target: 'serverless',
 };
 
 export default nextConfig;
